2fc256cee66a73cf2f8d5da1f51849dfa1c4af16
yeonha99 <rhdqn1207@naver.com>
2021-11-29 10:57:41 +0900

    feat : 스웨거 헤더 추가 기능

--- src/main/java/com/example/userservice/config/SwaggerConfig.java
+++ src/main/java/com/example/userservice/config/SwaggerConfig.java
@@ -2,24 +2,57 @@ package com.example.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
 
     @Bean
     public Docket api() {
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("Authorization") //헤더 이름
+                .description("Authorization token") //설명
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
+
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(aParameters)
                 .select()
                 .apis(RequestHandlerSelectors.any()) // RequestMapping으로 할당된 모든 URL 리스트를 추출
                 // 그중 /api/** 인 URL들만 필터링
                 .paths(PathSelectors.ant("/user-service/**"))
                 .build();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
 }
 
