bac7209c02cfa4da7b7d7b321ee12ceff0692ef2
EC2 Default User <ec2-user@ip-172-16-2-117.ap-northeast-2.compute.internal>
2021-12-03 09:50:08 +0900

    fix: discovery ip 수정, deploy.sh수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
-sudo docker ps -a -q --filter "name=userservice" | grep -q . && docker stop userservice && docker rm userservice | true
-sudo docker rmi 0826hihi/up3-user-test:1.0
-sudo docker pull 0826hihi/up3-user-test:1.0
-sudo docker run -d -p 1000:1000 --name authserver 0826hihi/up3-user-test:1.0
-
+sudo docker ps -a -q --filter "name=user" | grep -q . && docker stop user && docker rm user | true
+sudo docker rmi 0826hihi/up3-user-test:latest
+sudo docker pull 0826hihi/up3-user-test:latest
+docker run -d -p 1000:1000 --name user 0826hihi/up3-user-test:latest
+docker rmi -f $(docker images -f "dangling=true" -q) || true
 
