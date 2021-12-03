3b447d3341f54c79c5228d68340c29030ced2b32
yeonha99 <60427107+yeonha99@users.noreply.github.com>
2021-12-03 17:49:23 +0900

    Update GeneralManagerController.java

--- src/main/java/com/example/userservice/user/controller/GeneralManagerController.java
+++ src/main/java/com/example/userservice/user/controller/GeneralManagerController.java
@@ -11,7 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user-service")
 @RequiredArgsConstructor
