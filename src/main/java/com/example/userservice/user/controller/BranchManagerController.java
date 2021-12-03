98af1bfc23c7751af647a60c913b87d11ba0dc82
yeonha99 <60427107+yeonha99@users.noreply.github.com>
2021-12-03 17:48:51 +0900

    Update BranchManagerController.java

--- src/main/java/com/example/userservice/user/controller/BranchManagerController.java
+++ src/main/java/com/example/userservice/user/controller/BranchManagerController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user-service")
 @RequiredArgsConstructor
