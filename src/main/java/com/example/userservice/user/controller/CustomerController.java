aa1c55db5943a9d180b0de68c363df3eb583a3db
yeonha99 <60427107+yeonha99@users.noreply.github.com>
2021-12-03 17:49:06 +0900

    Update CustomerController.java

--- src/main/java/com/example/userservice/user/controller/CustomerController.java
+++ src/main/java/com/example/userservice/user/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user-service")
 @RequiredArgsConstructor
